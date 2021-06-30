1794d140d1d0383a98d310c3267899deba10228f
Kaushal Regmi <kaushal.oksoftintl@gmail.com>
2021-06-30 09:20:30 +0545

    Fixed Dashboard Service

--- apps/tekkon-trial-backend/src/app/queries/DashboardQueries.ts
+++ apps/tekkon-trial-backend/src/app/queries/DashboardQueries.ts
@@ -1,4 +1,4 @@
-import {GraphQLID,GraphQLString,GraphQLInt,GraphQLObjectType,GraphQLList} from 'graphql';
+import {GraphQLID,GraphQLString,GraphQLInt,GraphQLObjectType,GraphQLList,GraphQLNonNull} from 'graphql';
 import {getDashboardList} from '../services';
 const DashType: GraphQLObjectType= new GraphQLObjectType({
     name: "Dashboard",
@@ -14,9 +14,19 @@ export const DashRootQueries = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
        dashboard: {
+        args:{
+            limit:{
+                type:new GraphQLNonNull(GraphQLInt),
+            },
+            offset:{
+                type:new GraphQLNonNull(GraphQLInt)
+            }
+        },
         type: new GraphQLList(DashType),
         resolve(parent, args) {
-          return getDashboardList()
+          const limit = args.limit||5;
+          const offset= args.offset||1;
+          return getDashboardList(limit,offset)
         },
       },
     },
--- apps/tekkon-trial-backend/src/app/services/DashboardServices.ts
+++ apps/tekkon-trial-backend/src/app/services/DashboardServices.ts
@@ -1,2 +1,6 @@
 import {DASH_LIST} from '../constants'
-export const getDashboardList = () => DASH_LIST
+export const getDashboardList:Function = (limit,offset) => {
+    const pageIndex =limit*(offset-1);
+    const sizeLimit = Number(limit* offset)
+    return DASH_LIST.slice(pageIndex,sizeLimit)
+}
